f4d88fc1d49945362803b9c263f28c52ef08a051
creammtk <montakarncream2008@gmail.com>
2022-12-01 15:37:06 +0700

    total credit used page done (static)

---
+++ montakarn_final_app/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:final_app/credit_card_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return DefaultTabController(
+      length: 2, 
+      child: Scaffold(
+      appBar: AppBar(
+       // title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.circular(25.0)
+              ),
+              child: TabBar(
+                indicator: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(25.0)
+                ),
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.black,
+                tabs: [
+                  Tab(text: 'Wallet',),
+                  Tab(text: 'Activity',)
+                ],
+              ),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  CreditCardPage(),
+                  Center(child: Text('Activity'),)
+                ],
+              )
+            )
+          ],
+        ),
+      )
+    )
+    );
+    
+  }
+}
